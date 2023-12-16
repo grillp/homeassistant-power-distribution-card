6338ed98550e9d091669b93c7f4c74a71e5ce4cc
Gil Peeters <gpeeters@seek.com.au>
2023-12-16 07:31:14 +0000

    Added Section titles to editor

--- dist/card.js
+++ dist/card.js
@@ -9216,7 +9216,7 @@ class $d067581fc0d59830$export$6820950cdde5f40e extends (0, $ab210b2da7b39b9d$ex
     render() {
         return (0, $f58f44579a4747ac$export$c0bb0b647f701bb5)`
             <form class="table">
-
+                <div class="row"><h2>Power</h2></div>
                 <div class="row"><label class="label cell" for="grid_to_house_id">grid_to_house_id:</label><input @change="${this.handleChangedEvent}" id="grid_to_house_id" value="${this._config.grid_to_house_id}"></input></div>
                 <div class="row"><label class="label cell" for="generation_to_grid_id">generation_to_grid_id:</label><input @change="${this.handleChangedEvent}" id="generation_to_grid_id" value="${this._config.generation_to_grid_id}"></input></div>
                 <div class="row"><label class="label cell" for="generation_to_battery_id">generation_to_battery_id:</label><input @change="${this.handleChangedEvent}" id="generation_to_battery_id" value="${this._config.generation_to_battery_id}"></input></div>
@@ -9225,12 +9225,15 @@ class $d067581fc0d59830$export$6820950cdde5f40e extends (0, $ab210b2da7b39b9d$ex
                 <div class="row"><label class="label cell" for="battery_to_grid_id">battery_to_grid_id:</label><input @change="${this.handleChangedEvent}" id="battery_to_grid_id" value="${this._config.battery_to_grid_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance1_power_id">appliance1_power_id:</label><input @change="${this.handleChangedEvent}" id="appliance1_power_id" value="${this._config.appliance1_power_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance2_power_id">appliance2_power_id:</label><input @change="${this.handleChangedEvent}" id="appliance2_power_id" value="${this._config.appliance2_power_id}"></input></div>
+                <div class="row"><h2>Extra Info</h2></div>
                 <div class="row"><label class="label cell" for="grid_info_id">grid_info_id:</label><input @change="${this.handleChangedEvent}" id="grid_info_id" value="${this._config.grid_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="battery_info_id">battery_info_id:</label><input @change="${this.handleChangedEvent}" id="battery_info_id" value="${this._config.battery_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="house_info_id">house_info_id:</label><input @change="${this.handleChangedEvent}" id="house_info_id" value="${this._config.house_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="generation_info_id">generation_info_id:</label><input @change="${this.handleChangedEvent}" id="generation_info_id" value="${this._config.generation_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance1_info_id">appliance1_info_id:</label><input @change="${this.handleChangedEvent}" id="appliance1_info_id" value="${this._config.appliance1_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance2_info_id">appliance2_info_id:</label><input @change="${this.handleChangedEvent}" id="appliance2_info_id" value="${this._config.appliance2_info_id}"></input></div>
+                <div class="row"><h2>Names</h2></div>
+                <div class="row"><h2>Icons</h2></div>
                 <div class="row"><label class="label cell" for="generation_icon">generation_icon:</label><input @change="${this.handleChangedEvent}" id="generation_icon" value="${this._config.generation_icon}"></input></div>
 
                 </form>
--- src/editor.ts
+++ src/editor.ts
@@ -24,7 +24,7 @@ export class TestlaPowerDistributionEditor extends LitElement {
   render() {
     return html`
             <form class="table">
-
+                <div class="row"><h2>Power</h2></div>
                 <div class="row"><label class="label cell" for="grid_to_house_id">grid_to_house_id:</label><input @change="${this.handleChangedEvent}" id="grid_to_house_id" value="${this._config.grid_to_house_id}"></input></div>
                 <div class="row"><label class="label cell" for="generation_to_grid_id">generation_to_grid_id:</label><input @change="${this.handleChangedEvent}" id="generation_to_grid_id" value="${this._config.generation_to_grid_id}"></input></div>
                 <div class="row"><label class="label cell" for="generation_to_battery_id">generation_to_battery_id:</label><input @change="${this.handleChangedEvent}" id="generation_to_battery_id" value="${this._config.generation_to_battery_id}"></input></div>
@@ -33,12 +33,15 @@ export class TestlaPowerDistributionEditor extends LitElement {
                 <div class="row"><label class="label cell" for="battery_to_grid_id">battery_to_grid_id:</label><input @change="${this.handleChangedEvent}" id="battery_to_grid_id" value="${this._config.battery_to_grid_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance1_power_id">appliance1_power_id:</label><input @change="${this.handleChangedEvent}" id="appliance1_power_id" value="${this._config.appliance1_power_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance2_power_id">appliance2_power_id:</label><input @change="${this.handleChangedEvent}" id="appliance2_power_id" value="${this._config.appliance2_power_id}"></input></div>
+                <div class="row"><h2>Extra Info</h2></div>
                 <div class="row"><label class="label cell" for="grid_info_id">grid_info_id:</label><input @change="${this.handleChangedEvent}" id="grid_info_id" value="${this._config.grid_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="battery_info_id">battery_info_id:</label><input @change="${this.handleChangedEvent}" id="battery_info_id" value="${this._config.battery_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="house_info_id">house_info_id:</label><input @change="${this.handleChangedEvent}" id="house_info_id" value="${this._config.house_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="generation_info_id">generation_info_id:</label><input @change="${this.handleChangedEvent}" id="generation_info_id" value="${this._config.generation_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance1_info_id">appliance1_info_id:</label><input @change="${this.handleChangedEvent}" id="appliance1_info_id" value="${this._config.appliance1_info_id}"></input></div>
                 <div class="row"><label class="label cell" for="appliance2_info_id">appliance2_info_id:</label><input @change="${this.handleChangedEvent}" id="appliance2_info_id" value="${this._config.appliance2_info_id}"></input></div>
+                <div class="row"><h2>Names</h2></div>
+                <div class="row"><h2>Icons</h2></div>
                 <div class="row"><label class="label cell" for="generation_icon">generation_icon:</label><input @change="${this.handleChangedEvent}" id="generation_icon" value="${this._config.generation_icon}"></input></div>
 
                 </form>
